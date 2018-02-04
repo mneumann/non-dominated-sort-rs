71e9bf03880ee0f3c8a5964d77e49799dde7d542
Michael Neumann <mneumann@ntecs.de>
2018-02-04 17:42:57 +0100

    Refactor the code. Use less tuples.

--- src/non_dominated_sort.rs
+++ src/non_dominated_sort.rs
@@ -3,6 +3,39 @@ use std::cmp::Ordering;
 use std::mem;
 use std::collections::VecDeque;
 
+pub struct SolutionWithIndex<'a, S, I: Copy = usize>
+where
+    S: 'a,
+{
+    /// A reference to the solution
+    solution: &'a S,
+
+    /// The index that `solution` has within the `solutions` array.
+    index: I,
+}
+
+pub struct Front<'a, S, I = usize>
+where
+    S: 'a,
+    I: Copy,
+{
+    /// The first front has rank 0, second rank 1 and so on.
+    rank: usize,
+
+    // The solutions within this front
+    solutions: Vec<SolutionWithIndex<'a, S, I>>,
+}
+
+impl<'a, S, I> Front<'a, S, I>
+where
+    S: 'a,
+    I: Copy,
+{
+    pub fn solutions_indices_only(&self) -> Vec<I> {
+        self.solutions.iter().map(|s| s.index).collect()
+    }
+}
+
 struct Entry<'a, S, I = usize>
 where
     S: 'a,
@@ -20,12 +53,12 @@ where
     dominated_solutions: VecDeque<I>,
 }
 
-pub struct NonDominatedSort<'a, S, I = usize>
+pub struct NonDominatedSort<'a, S, I: Copy = usize>
 where
     S: 'a,
 {
     entries: Vec<Entry<'a, S, I>>,
-    current_front: Vec<(I, &'a S)>,
+    current_front: Front<'a, S, I>,
 }
 
 impl<'a, S> NonDominatedSort<'a, S> {
@@ -37,7 +70,10 @@ impl<'a, S> NonDominatedSort<'a, S> {
     where
         D: DominationOrd<Solution = S>,
     {
-        let mut current_front = Vec::new();
+        let mut current_front = Front {
+            rank: 0,
+            solutions: Vec::new(),
+        };
 
         let mut entries: Vec<_> = solutions
             .iter()
@@ -77,7 +113,10 @@ impl<'a, S> NonDominatedSort<'a, S> {
             if p.domination_count == 0 {
                 // `p` belongs to the first front as it is not dominated by any
                 // other solution.
-                current_front.push((p.index, p.solution));
+                current_front.solutions.push(SolutionWithIndex {
+                    solution: p.solution,
+                    index: p.index,
+                });
             }
         }
 
@@ -88,23 +127,19 @@ impl<'a, S> NonDominatedSort<'a, S> {
     }
 
     /// Returns an array containing all pareto fronts.
-    pub fn pareto_fronts(self) -> Vec<Vec<(usize, &'a S)>> {
-        let mut fronts = Vec::new();
-        for front in self {
-            fronts.push(front);
-        }
-        return fronts;
+    pub fn pareto_fronts(self) -> Vec<Front<'a, S, usize>> {
+        self.into_iter().collect()
     }
 
     /// Returns an array containing the first pareto fronts, until
     /// `max_solutions` have been found. Note that always the whole fronts are
     /// returned, i.e. the number of solutions returned may be higher.
-    pub fn pareto_fronts_stop_at(self, max_solutions: usize) -> Vec<Vec<(usize, &'a S)>> {
+    pub fn pareto_fronts_stop_at(self, max_solutions: usize) -> Vec<Front<'a, S, usize>> {
         let mut found_solutions = 0;
         let mut fronts = Vec::new();
 
         for front in self {
-            found_solutions += front.len();
+            found_solutions += front.solutions.len();
             fronts.push(front);
             if found_solutions >= max_solutions {
                 break;
@@ -117,7 +152,7 @@ impl<'a, S> NonDominatedSort<'a, S> {
 /// Iterate over the pareto fronts. Each call to next() will yield the
 /// next pareto front.
 impl<'a, S> Iterator for NonDominatedSort<'a, S> {
-    type Item = Vec<(usize, &'a S)>;
+    type Item = Front<'a, S, usize>;
 
     /// Return the next pareto front
 
@@ -126,9 +161,12 @@ impl<'a, S> Iterator for NonDominatedSort<'a, S> {
         // might be empty, in which case the next_front will be empty as
         // well and we stop.
 
-        let mut next_front = Vec::new();
+        let mut next_front = Front {
+            rank: self.current_front.rank + 1,
+            solutions: Vec::new(),
+        };
 
-        for &(p_i, _) in self.current_front.iter() {
+        for p in self.current_front.solutions.iter() {
             // to calculate the next front, we have to remove the
             // solutions of the current front, and as such, decrease the
             // domination_count of they dominated_solutions. We can
@@ -139,13 +177,16 @@ impl<'a, S> Iterator for NonDominatedSort<'a, S> {
             // that reason, we are using a VecDeque. This should give us
             // a stable sort.
 
-            while let Some(q_i) = self.entries[p_i].dominated_solutions.pop_front() {
+            while let Some(q_i) = self.entries[p.index].dominated_solutions.pop_front() {
                 let q = &mut self.entries[q_i];
                 debug_assert!(q.domination_count > 0);
                 q.domination_count -= 1;
                 if q.domination_count == 0 {
                     // q is not dominated by any other solution. it belongs to the next front.
-                    next_front.push((q_i, q.solution));
+                    next_front.solutions.push(SolutionWithIndex {
+                        solution: q.solution,
+                        index: q_i,
+                    });
                 }
             }
         }
@@ -153,7 +194,7 @@ impl<'a, S> Iterator for NonDominatedSort<'a, S> {
         // swap current with next front
         let current_front = mem::replace(&mut self.current_front, next_front);
 
-        if current_front.is_empty() {
+        if current_front.solutions.is_empty() {
             None
         } else {
             Some(current_front)
@@ -164,6 +205,7 @@ impl<'a, S> Iterator for NonDominatedSort<'a, S> {
 #[cfg(test)]
 mod helper {
     use test_helper_domination::Tuple;
+    use super::Front;
     pub fn get_solutions() -> Vec<Tuple> {
         vec![
             Tuple(1, 2),
@@ -173,12 +215,14 @@ mod helper {
             Tuple(0, 2),
         ]
     }
-    pub fn keep_only_index(front: &[(usize, &Tuple)]) -> Vec<usize> {
-        front.iter().map(|&(i, _)| i).collect()
-    }
 
-    pub fn assert_front_eq(expected: &[usize], front: &[(usize, &Tuple)]) {
-        assert_eq!(expected.to_owned(), keep_only_index(front));
+    pub fn assert_front_eq(
+        expected_rank: usize,
+        expected_indices: &[usize],
+        front: &Front<Tuple, usize>,
+    ) {
+        assert_eq!(expected_rank, front.rank);
+        assert_eq!(expected_indices.to_owned(), front.solutions_indices_only());
     }
 }
 
@@ -189,9 +233,9 @@ fn test_non_dominated_sort() {
     let fronts = NonDominatedSort::new(&solutions, &TupleDominationOrd).pareto_fronts();
 
     assert_eq!(3, fronts.len());
-    helper::assert_front_eq(&[2, 4], &fronts[0]);
-    helper::assert_front_eq(&[0, 1], &fronts[1]);
-    helper::assert_front_eq(&[3], &fronts[2]);
+    helper::assert_front_eq(0, &[2, 4], &fronts[0]);
+    helper::assert_front_eq(1, &[0, 1], &fronts[1]);
+    helper::assert_front_eq(2, &[3], &fronts[2]);
 }
 
 #[test]
@@ -203,14 +247,14 @@ fn test_non_dominated_sort_stop_at() {
         let fronts =
             NonDominatedSort::new(&solutions, &TupleDominationOrd).pareto_fronts_stop_at(2);
         assert_eq!(1, fronts.len());
-        helper::assert_front_eq(&[2, 4], &fronts[0]);
+        helper::assert_front_eq(0, &[2, 4], &fronts[0]);
     }
     {
         let fronts =
             NonDominatedSort::new(&solutions, &TupleDominationOrd).pareto_fronts_stop_at(3);
         assert_eq!(2, fronts.len());
-        helper::assert_front_eq(&[2, 4], &fronts[0]);
-        helper::assert_front_eq(&[0, 1], &fronts[1]);
+        helper::assert_front_eq(0, &[2, 4], &fronts[0]);
+        helper::assert_front_eq(1, &[0, 1], &fronts[1]);
     }
 }
 
@@ -219,10 +263,24 @@ fn test_non_dominated_sort_iter() {
     use test_helper_domination::TupleDominationOrd;
     let solutions = helper::get_solutions();
     let mut fronts = NonDominatedSort::new(&solutions, &TupleDominationOrd);
-    let f = &|front: Vec<_>| helper::keep_only_index(&front);
 
-    assert_eq!(Some(vec![2, 4]), fronts.next().map(f));
-    assert_eq!(Some(vec![0, 1]), fronts.next().map(f));
-    assert_eq!(Some(vec![3]), fronts.next().map(f));
-    assert_eq!(None, fronts.next().map(f));
+    {
+        let f0 = fronts.next();
+        assert!(f0.is_some());
+        helper::assert_front_eq(0, &[2, 4], &f0.unwrap());
+    }
+    {
+        let f1 = fronts.next();
+        assert!(f1.is_some());
+        helper::assert_front_eq(1, &[0, 1], &f1.unwrap());
+    }
+    {
+        let f2 = fronts.next();
+        assert!(f2.is_some());
+        helper::assert_front_eq(2, &[3], &f2.unwrap());
+    }
+    {
+        let f3 = fronts.next();
+        assert!(f3.is_none());
+    }
 }
